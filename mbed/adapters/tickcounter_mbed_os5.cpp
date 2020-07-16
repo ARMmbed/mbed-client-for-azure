137ac4013f02567ae7aa8028f95482e91658ea74
Lingkai Dong <lingkai.dong@arm.com>
2020-07-16 17:34:39 +0100

    Add Arm Limited to copyright list for adapter code we updated

--- mbed/adapters/tickcounter_mbed_os5.cpp
+++ mbed/adapters/tickcounter_mbed_os5.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) 2020 ARM Limited
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include "mbed.h"
